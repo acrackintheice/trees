36a53bb30730981de3784188d9b0b63d3a6c900e
Eduardo Demeneck Onghero <acrackintheice@gmail.com>
2019-12-15 18:15:29 -0300

    Implementa a inserção de itens na árvore

--- include/tree.h
+++ include/tree.h
@@ -6,44 +6,15 @@
 
 #include <iostream>
 
-// Element Definitions
-namespace Tree_Elements
+// Node Definitions
+namespace Tree_Nodes
 {
 template <typename T>
-class Element
+class Red_Black_Node
 {
 public:
-    Element(int key, T *data) : _left(0), _right(0), _parent(0), _key(key), _data(data) {}
+    typedef Red_Black_Node<T> Node;
 
-    int key() const { return _key; }
-
-    T *data() const { return const_cast<T *>(_data); }
-
-    Element *left() const { return _left; }
-
-    Element *right() const { return _right; }
-
-    Element *parent() const { return _parent; }
-
-    void left(Element *e) { this->_left = e; }
-
-    void right(Element *e) { this->_right = e; }
-
-    void parent(Element *e) { this->_parent = e; }
-
-    virtual std::string to_string() = 0;
-
-protected:
-    Element *_left;
-    Element *_right;
-    Element *_parent;
-    int _key;
-    T *_data;
-};
-
-template <typename T>
-class Red_Black_Element : public Element<T>
-{
 public:
     enum Color
     {
@@ -52,131 +23,148 @@ public:
     };
 
 public:
-    Red_Black_Element(int key, T *data) : Element<T>(key, data), _color(RED){};
+    Red_Black_Node(int key, T *data) : _left(0), _right(0), _key(key), _data(data), _color(RED){};
+
+    void flip_color()
+    {
+        if (this->_color == RED)
+            this->_color = BLACK;
+        else
+            this->_color = RED;
+    }
+
+    void color(Color c) { this->_color = c; }
+
+    Color color() { return this->_color; }
+
+    bool is_red() { return this->_color == RED; }
+
+    int key() const { return _key; }
+
+    T *data() const { return const_cast<T *>(_data); }
+
+    Node *left() const { return _left; }
+
+    Node *right() const { return _right; }
+
+    void data(T *data) { this->_data = data; }
+
+    void left(Node *h) { this->_left = h; }
+
+    void right(Node *h) { this->_right = h; }
 
     std::string to_string()
     {
-        return std::string("Something");
+        return std::string("[") + (this->_left == 0 ? std::string("[0]") : this->_left->to_string()) +
+               std::string(" <- [") + std::to_string(this->_key) + std::string(" - ") + (_color == RED ? std::string("RED") : std::string("BLACK")) + std::string("] -> ") +
+               (this->_right == 0 ? std::string("[0]") : this->_right->to_string()) + std::string("]");
     }
 
 private:
+    Node *_left;
+    Node *_right;
+    int _key;
+    T *_data;
     Color _color;
 };
-} // namespace Tree_Elements
+} // namespace Tree_Nodes
 
 // Tree Definitions
-template <typename T>
-class Tree
-{
-public:
-    typedef Tree_Elements::Element<T> Element;
-
-public:
-    Tree() : _root(0) {}
-
-    void root(Element *e) { this->_root = e; }
-
-    virtual Element *search(int key)
-    {
-        return this->search_recursive(key, this->_root);
-    };
-
-    virtual void insert(int key, T * data) = 0;
-
-    virtual Element *remove(int key) = 0;
-
-    std::string to_string()
-    {
-        return _root->to_string();
-    }
-
-protected:
-    virtual Element *search_recursive(int key, Element *e)
-    {
-        if (e->key() == key)
-            return e;
-        if (e->key() > key)
-            return e->left() ? search_recursive(key, e->left()) : 0;
-        else
-            return e->right() ? search_recursive(key, e->right()) : 0;
-    }
-
-    virtual void insert_recursive(Element *e, Element *n)
-    {
-        // Root is null
-        if (e == 0) {
-            this->root(n);
-        }
-        // <n> must be inserted in <e>'s left tree
-        else if (e->key() > n->key())
-        {
-            // <n> must be inserted as <e>'s left child
-            if (e->left() == 0)
-            {
-                e->left(n);
-                n->parent(e);
-            }
-            else
-                insert_recursive(e->left(), n);
-        }
-        // <n> must be inserted in <e>'s right tree
-        else
-        {
-            // <n> must be inserted as <e>'s right child
-            if (e->right() == 0)
-            {
-                e->right(n);
-                n->parent(e);
-            }
-            else
-                insert_recursive(e->right(), n);
-        }
-    }
-
-protected:
-    Element *_root;
-};
-
+// Left leaning Red-Black-Tree implementation based on: https://www.cs.princeton.edu/~rs/talks/LLRB/LLRB.pdf
 template <typename T,
-          typename El = Tree_Elements::Red_Black_Element<T>>
-class Red_Black : public Tree<T>
+          typename El = Tree_Nodes::Red_Black_Node<T>>
+class Red_Black
 {
 public:
-    typedef El Element;
+    typedef El Node;
 
 public:
-    Red_Black() {}
+    Red_Black() : _root(0) {}
 
-    void insert(int key, T * data)
-    {
-        this->insert_recursive(this->_root, new Element(key, data));
-        // TODO - Balance the tree after insertions
-    };
+    void root(Node *h) { this->_root = h; }
 
-    Element *remove(int key)
+    Node *search(int key) { return this->search_recursive(key, this->_root); };
+
+    Node *remove(int key) { return 0; };
+
+    std::string to_string() { return _root->to_string(); }
+
+    void insert(int key, T *data)
     {
-        return 0;
+        this->_root = this->insert_recursive(this->_root, new Node(key, data));
+        this->_root->color(Node::BLACK);
     };
 
 private:
-    void rotate_left(Element *b)
+    Node *search_recursive(int key, Node *h)
     {
-        Element *a = b->right();
-        Element *d = a->left();
-        a->parent(b->parent());
-        b->parent(a);
-        a->left(b);
-        b->right(d);
-    };
-    void rotate_right(Element *a)
+        if (h->key() == key)
+            return h;
+        if (h->key() > key)
+            return h->left() ? search_recursive(key, h->left()) : 0;
+        else
+            return h->right() ? search_recursive(key, h->right()) : 0;
+    }
+    Node *insert_recursive(Node *h, Node *n)
     {
-        Element *b = a->left();
-        Element *d = b->right();
-        b->parent(a->parent());
-        a->parent(b);
-        b->right(a);
-        a->left(d);
+        if (h == 0)
+            return n;
+
+        if (is_red(h->left()) && is_red(h->right()))
+            this->flip_colors(h);
+
+        if (h->key() == n->key())
+            h->data(n->data());
+
+        if (n->key() < h->key())
+            h->left(insert_recursive(h->left(), n));
+        else
+            h->right(insert_recursive(h->right(), n));
+
+        if (is_red(h->right()) && !is_red(h->left()))
+            h = rotate_left(h);
+        if (is_red(h->left()) && is_red(h->left()->left()))
+            h = rotate_right(h);
+
+        return h;
+    }
+
+    Node *rotate_left(Node *h)
+    {
+        Node *x = h->right();
+        h->right(x->left());
+        x->left(h);
+        x->color(h->color());
+        h->color(Node::RED);
+        return x;
     };
+
+    Node *rotate_right(Node *h)
+    {
+        Node *x = h->left();
+        h->left(x->right());
+        x->right(h);
+        x->color(h->color());
+        h->color(Node::RED);
+        return x;
+    };
+    void flip_colors(Node *h)
+    {
+        h->flip_color();
+        if (h->left())
+            h->left()->flip_color();
+        if (h->right())
+            h->right()->flip_color();
+
+    }
+    bool is_red(Node *h)
+    {
+        return h == 0 ? 0 : h->is_red();
+    }
+
+private:
+protected:
+    Node *_root;
 };
 
 #endif
--- src/application.cpp
+++ src/application.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include "../include/tree.h"
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     typedef Red_Black<std::string> StringTree;
     StringTree tree = StringTree();
+    tree.insert(1, new std::string("Element 1"));
+    tree.insert(3, new std::string("Element 3"));
+    tree.insert(5, new std::string("Element 5"));
+    tree.insert(8, new std::string("Element 8"));
+    tree.insert(10, new std::string("Element 10"));
     tree.insert(12, new std::string("Element 12"));
-    tree.insert(15, new std::string("Element 15"));
+    tree.insert(13, new std::string("Element 13"));
+    tree.insert(16, new std::string("Element 16"));
+    tree.insert(18, new std::string("Element 18"));
+    tree.insert(19, new std::string("Element 19"));
+    tree.insert(24, new std::string("Element 24"));
+    tree.insert(26, new std::string("Element 26"));
 
     std::cout << "Tress are great" << std::endl;
-    std::cout << "This is my Tree: \n" << tree.to_string() << std::endl; 
-    std::cout << "This is my Tree: \n" << *(tree.search(15)->data()) << std::endl; 
+    std::cout << "This is my Tree: \n" << tree.to_string() << std::endl;
+    std::cout << "Search for 5: \n" << ((tree.search(5)->color() == Tree_Nodes::Red_Black_Node<std::string>::Color::RED) ? std::string("RED") : std::string("Black")) << std::endl;
+
     return 0;
-
 }
-
